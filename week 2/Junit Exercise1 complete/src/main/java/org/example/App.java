daee638a0f7a66fe875089ce9522ecd909b3e4e5
Saksham Chaturvedi <your-email@gmail.com>
2026-07-07 10:27:22 +0530

    DONE

---
+++ week 2/Junit Exercise1 complete/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+// Removed package declaration to match file location
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
