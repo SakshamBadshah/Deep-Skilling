92f262aab4a598df248957353b5dbc4a7ca3fc3d
Saksham Chaturvedi <your-email@gmail.com>
2026-07-19 11:53:40 +0530

    DONE

---
+++ week4/02_Spring_rest_Handson/02_REST_Get_country_based_on_country_code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,18 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
---
+++ week4/02_Spring_rest_Handson/02_REST_Get_country_based_on_country_code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.cognizant.spring_learn.controller;
+
+
+public class CountryService {
+
+    @SuppressWarnings("unused")
+    public Country getCountry(String code) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
