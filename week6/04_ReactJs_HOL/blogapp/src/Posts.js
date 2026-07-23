a6c2e108acad3eb264a392693518279b76d57301
Saksham Chaturvedi <your-email@gmail.com>
2026-07-23 15:19:37 +0530

    DONE

---
+++ week6/04_ReactJs_HOL/blogapp/src/Posts.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    // Load posts using Fetch API
+    loadPosts = () => {
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => {
+
+                const postList = data.map(
+                    item => new Post(item.id, item.title, item.body)
+                );
+
+                this.setState({
+                    posts: postList
+                });
+
+            })
+            .catch(error => {
+                alert(error);
+            });
+
+    }
+
+    // Lifecycle Hook
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    // Error Handling
+    componentDidCatch(error, info) {
+        alert(error);
+    }
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h1>Posts</h1>
+
+                {
+
+                    this.state.posts.map(post => (
+
+                        <div key={post.id}>
+
+                            <h3>{post.title}</h3>
+
+                            <p>{post.body}</p>
+
+                            <hr />
+
+                        </div>
+
+                    ))
+
+                }
+
+            </div>
+
+        );
+    }
+}
+
+export default Posts;
