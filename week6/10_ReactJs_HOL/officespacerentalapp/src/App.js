f1029a3cc6ba35719e1e207f1aa1688e5e636153
Saksham Chaturvedi <your-email@gmail.com>
2026-07-24 22:17:58 +0530

    DONE

---
+++ week6/10_ReactJs_HOL/officespacerentalapp/src/App.js
@@ -0,0 +1,36 @@
+function App() {
+
+  // Office Object
+  const office = {
+    name: "DBS Business Center",
+    rent: 55000,
+    address: "Chennai"
+  };
+
+  // Array of Office Objects
+  const officeList = [
+    {
+      name: "DBS Business Center",
+      rent: 55000,
+      address: "Chennai"
+    },
+    {
+      name: "WeWork",
+      rent: 75000,
+      address: "Bangalore"
+    },
+    {
+      name: "Regus",
+      rent: 45000,
+      address: "Hyderabad"
+    }
+  ];
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
